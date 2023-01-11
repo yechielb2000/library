6992c23e347aad6fba6a5ca681e33faa57b0c936
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2023-01-11 22:35:32 +0200

    adding material-ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import SearchAppBar from './components/SearchAppBar';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-   'Hello World'
+   <div className='app'>
+	<SearchAppBar/>
+		<Routes>
+			<Route path="/" element='Hi'/>
+			<Route path="/book/:id" element='Bye'/>
+			<Route path="/favorites" element=''/>	
+		</Routes>
+   </div>
   );
 }
 
