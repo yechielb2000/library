645d83eab520e5c529c55362db91bc1c518a22f5
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2023-01-16 20:18:31 +0200

    extracting search from bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-import SearchAppBar from './components/SearchAppBar';
+import Bar from './components/Bar';
 import BookList from './components/BookList';
 import Book from './components/Book';
 
 function App() {
   return (
-   <div>
-	<SearchAppBar/>
+   <>
+	<Bar/>
 		<Routes>
 			<Route path="/" element={ <BookList/> }/>
 			<Route path="/book/:id" element= { <Book/> }/>
 			<Route path="/favorites" element= { <BookList/> }/>
 			<Route path="/already-read" element= { <BookList/> }/>	
 		</Routes>
-   </div>
+   </>
   );
 }
 
