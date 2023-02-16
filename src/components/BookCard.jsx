577b243736543695d97bf6ba656892932943c091
Raja Poudel <rajapoudel342@gmail.com>
2023-02-16 20:18:50 +0630

    Make some changes related each item  dialog box

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import useParams from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
